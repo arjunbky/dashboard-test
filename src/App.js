d02c04c75a8c90dea4a0f9887fa698643800b854
arjunbky <arjunbky@gmail.com>
2024-03-07 16:16:01 -0600

    new update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Home from "./pages/homed3js/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Reports  from "./pages/reports/Reports";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/reports" element={<Reports />}/>
+          {/* <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
